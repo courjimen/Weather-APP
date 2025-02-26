86b7dfff581476c279f816b2729ca4715105a063
courjimen <coureyfrance@gmail.com>
2025-02-26 16:38:37 -0500

    used params to connect api and display weather

--- server/server.js
+++ server/server.js
@@ -9,15 +9,15 @@ const PORT = 5000;
 
 //creates an endpoint for the route /api
 app.get('/weather', (req, res) => {
-  const city = 'Detroit';
+  const city = req.query.Detroit;
   const apiKey = process.env.API_KEY;
-  // const params = new URLSearchParams({
-  //   q: Detroit,
-  //   appid: apiKey,
-  //   units: "imperial",
-  // });
+  const params = new URLSearchParams({
+    q: 'city',
+    appid: apiKey,
+    units: "imperial",
+  });
   
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;;
+const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;;
   console.log(url);
   fetch(url)
     .then((res) => res.json())
