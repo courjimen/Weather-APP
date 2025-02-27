60f567e7dda25736119c3dff86a004be969c02d9
courjimen <coureyfrance@gmail.com>
2025-02-26 23:50:05 -0500

    visually displayed message through vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     // add this code
     proxy: {
-      '/api': {
+      '/weather': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
@@ -17,10 +17,3 @@ export default defineConfig({
   }
 });
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
