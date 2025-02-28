4184d7f57422acdec4b842edac322c04ba30badf
courjimen <coureyfrance@gmail.com>
2025-02-28 00:06:02 -0500

    pulled child component to parent, created onChange func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 // client/src/App.jsx
-import { useState } from 'react';
+import React, { useState } from "react";
+import Weather from './Weather';
 
 function App() {
   const [data, setData] = useState(null);
   const [city, setCity] = useState('');
 
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  }
   const connectToBackend = () => {
-    fetch('/weather')
+    fetch('/weather/:city')
       .then((res) => res.json())
-      .then((data) => setData(data.city));
+      .then((data) => setData(data));
   };
 
   return (
@@ -17,7 +21,10 @@ function App() {
       <form>
       <h1>Weather in Detroit</h1>
       <input 
+      type = "text"
       placeholder='Insert City'
+      value={city}
+      onChange={handleInputChange}
     />
       <button onClick={connectToBackend}>Submit</button>
       </form>
@@ -30,5 +37,3 @@ function App() {
 }
 
 export default App;
-
-// export default App
