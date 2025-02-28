b9dfd15dfbe3d314e49c401323eb06c6dd0bdf1e
courjimen <coureyfrance@gmail.com>
2025-02-28 01:25:08 -0500

    removed unnecessary code

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
 import fetch from 'node-fetch';
-
 const app = express();
 const apiKey = process.env.API_KEY;
+
 //Set the port that you want the server to run on
 const PORT = 5000;
 
 //creates an endpoint for the route /api
 app.get('/weather/:city', async (req, res) => {
   const city = req.params.city;
-
+  console.log("city typed:", city);
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data);
 
     const description = data.weather[0].description;
     const temperature = data.main.temp;
