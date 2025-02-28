a1217024d72ddef7750f713b7f8abe3512335936
courjimen <coureyfrance@gmail.com>
2025-02-28 18:02:38 -0500

    added readme

--- client/src/components/NewWeather.jsx
+++ client/src/components/NewWeather.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function NewWeather({city, description, cloudy, temperature, iconUrl, windSpeed}) {
+function NewWeather({city, description, cloudy, temperature, iconUrl, windSpeed, humidity}) {
   return (
     <>
      <h2>City: {city}</h2>
      {iconUrl && <img src={iconUrl} alt="weather icon"/>}
       <p>Description: {description}</p>
-      <p>Clouds: ☁️ {cloudy.all}</p>
       <p>Temp: 🌡️ {temperature}</p>
+      <p>Humidity: 💧 {humidity}</p>
       <p>Wind: 💨 {windSpeed} mph</p>
+      <p>Clouds: ☁️ {cloudy.all}</p>
     </>
   )
 }
