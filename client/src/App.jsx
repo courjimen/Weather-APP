b1f4c695eab1ef310c3d9ff60d4bac732d8e657d
courjimen <coureyfrance@gmail.com>
2025-03-02 15:08:35 -0500

    added error handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,9 @@ import NewWeather from './components/NewWeather'
 function App() {
   const [data, setData] = useState(null);
   const [city, setCity] = useState('');
-  const [iconUrl, setIconUrl] = useState(null)
-
+  const [iconUrl, setIconUrl] = useState(null);
+  const [err, setErr] = useState(null);
+  
   const handleInputChange = (event) => {
     setCity(event.target.value);
   }
@@ -16,35 +17,39 @@ function App() {
     // handle errors
     fetch(`/weather/${city}`)
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .catch((err) => setErr("Invalid city. Try again."))
   };
 
+  // console.log("data: ", data);
   // added for icon visibility
   useEffect(() => {
-    if (data && data.weather  && data.weather.length > 0) {
+    if (data && data.weather && data.weather.length > 0) {
       const iconCode = data.weather[0].icon;
       const url = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
       setIconUrl(url);
     } else {
       setIconUrl(null);
     }
-  }, [data]); //run effect when data changes
-
+  }, [data, err]); //run effect when data changes
+  
+  console.log({data});
   return (
     <div className="App">
 
       <form onSubmit={connectToBackend}>
-      <h1>Weather in {city || "..."}</h1>
-      <input 
-      type = "text"
-      placeholder='Insert City'
-      value={city}
-      onChange={handleInputChange}
-    />
-      <button type="submit">Submit</button>
+        <h1>Weather in {city || "..."}</h1>
+        <input
+          type="text"
+          placeholder='Insert City'
+          value={city}
+          onChange={handleInputChange}
+          required
+        />
+        <button type="submit">Submit</button>
       </form>
-      
-     {data && <NewWeather {...data} iconUrl={iconUrl}/>}
+      {data && data.city && <NewWeather {...data} iconUrl={iconUrl} />}
+      { err && <div>{err}</div>}
 
     </div>
   );
